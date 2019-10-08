4042884508c4ee1132d5cd50e814b4c5dd723a08
daniaalrababa9 <rababadania@gmail.com>
2019-10-08 16:34:40 +0300

    add some features

--- js/add.js
+++ js/add.js
@@ -1,56 +1,56 @@
-'use strict';
-var name = prompt(' what is your name?');
-alert('Hi  ' + name + '  welcome to my bage ');
-//console.log('name', name);
-var food = prompt('do you eat junk food?');
-food = food.toLowerCase();
-if (food === 'yes') {
-    alert('that is bad for your health');
-} else if (food === 'y') {
-    alert('that is bad for your health');
-} else {
-    alert('good for you.');
-}
-//console.log('food', food);
-var smoking = prompt('do you smoke?');
-smoking = smoking.toLowerCase();
-if (smoking === 'yes') {
-    alert('no this is bad for your health, would you try to quit');
-} else if (smoking === 'y') {
-    alert('no this is bad for your health, would you try to quit');
-} else {
-    alert('good for you.');
-}
-//console.log('smoking', smoking);
-var exercises = prompt('do you do exercises?');
-exercises = exercises.toLowerCase();
-if (exercises === 'yes') {
-    alert('good ,keep going.');
-} else if (exercises === 'y') {
-    alert('good ,keep going.');
-} else {
-    alert('oh, no this is bad for your health.');
-}
-//console.log('exercises', exercises);
-var breakfast = prompt('do you eat brakfast?');
-breakfast = breakfast.toLowerCase();
-if (breakfast === 'yes') {
-    alert('great,keep going');
-} else if (breakfast === 'y') {
-    alert('great,keep going');
-} else {
-    alert('please avoid skipping breakfast ,lest your blood sugar drop.');
-}
-//console.log('breakfast', breakfast);
-var cover = prompt('do you cover your head during sleep ?');
-cover = cover.toLowerCase();
-if (cover === 'yes') {
-    alert('try to don\'t do it because it will Increases the concentration of carbon dioxide and reduces the concentration of oxygen, which leads to negative effects on the brain');
-} else if (cover === 'y') {
-    alert('try to don\'t do it because it will Increases the concentration of carbon dioxide and reduces the concentration of oxygen, which leads to negative effects on the brain');
-} else {
-    alert('good for you.');
-}
-//console.log('cover', cover);
-alert('Bye bye   ' + name + ', I hope that you enjoy visiting my site ');
+// 'use strict';
+// var name = prompt(' what is your name?');
+// alert('Hi  ' + name + '  welcome to my bage ');
+// //console.log('name', name);
+// var food = prompt('do you eat junk food?');
+// food = food.toLowerCase();
+// if (food === 'yes') {
+//     alert('that is bad for your health');
+// } else if (food === 'y') {
+//     alert('that is bad for your health');
+// } else {
+//     alert('good for you.');
+// }
+// //console.log('food', food);
+// var smoking = prompt('do you smoke?');
+// smoking = smoking.toLowerCase();
+// if (smoking === 'yes') {
+//     alert('no this is bad for your health, would you try to quit');
+// } else if (smoking === 'y') {
+//     alert('no this is bad for your health, would you try to quit');
+// } else {
+//     alert('good for you.');
+// }
+// //console.log('smoking', smoking);
+// var exercises = prompt('do you do exercises?');
+// exercises = exercises.toLowerCase();
+// if (exercises === 'yes') {
+//     alert('good ,keep going.');
+// } else if (exercises === 'y') {
+//     alert('good ,keep going.');
+// } else {
+//     alert('oh, no this is bad for your health.');
+// }
+// //console.log('exercises', exercises);
+// var breakfast = prompt('do you eat brakfast?');
+// breakfast = breakfast.toLowerCase();
+// if (breakfast === 'yes') {
+//     alert('great,keep going');
+// } else if (breakfast === 'y') {
+//     alert('great,keep going');
+// } else {
+//     alert('please avoid skipping breakfast ,lest your blood sugar drop.');
+// }
+// //console.log('breakfast', breakfast);
+// var cover = prompt('do you cover your head during sleep ?');
+// cover = cover.toLowerCase();
+// if (cover === 'yes') {
+//     alert('try to don\'t do it because it will Increases the concentration of carbon dioxide and reduces the concentration of oxygen, which leads to negative effects on the brain');
+// } else if (cover === 'y') {
+//     alert('try to don\'t do it because it will Increases the concentration of carbon dioxide and reduces the concentration of oxygen, which leads to negative effects on the brain');
+// } else {
+//     alert('good for you.');
+// }
+// //console.log('cover', cover);
+// alert('Bye bye   ' + name + ', I hope that you enjoy visiting my site ');
 
---
+++ js/new.js
@@ -0,0 +1,89 @@
+'use strict';
+var score = 0;
+var name = prompt(' what is your name?');
+alert('Hi  ' + name + '  welcome to my bage ');
+var dania = [['do you eat junk food?', 'that is bad for your health', 'good for you.']
+    , ['do you smoke?', 'no this is bad for your health, would you try to quit', 'good for you.']
+    , ['do you do exercises?', 'good ,keep going.', 'oh, no this is bad for your health.']
+    , ['do you eat brakfast?', 'great,keep going', 'please avoid skipping breakfast ,lest your blood sugar drop.']
+    , ['do you cover your head during sleep ?', 'try to don\'t do it because it will Increases the concentration of carbon dioxide and reduces the concentration of oxygen, which leads to negative effects on the brain', 'good for you.']
+]
+for (var i = 0; i < dania.length; i++) {
+    var daniaa = dania[i];
+    var userreply = prompt(daniaa[0]);
+    userreply = userreply.toLowerCase();
+    var yesreply = daniaa[1];
+    var noreply = daniaa[2];
+    switch (userreply) {
+        case 'yes':
+        case 'y':
+            alert(yesreply);
+            break;
+        case 'no':
+        case 'n':
+            alert(noreply);
+            break;
+        default:
+            alert('wrong answer');
+    }
+}
+var age = prompt('do you know how old iam? ');
+var num1 = parseInt(age);
+
+switch (num1) {
+    case 26:
+        alert('correct answer  iam ' + num1 + ' years old ');
+        score++;
+        break;
+    default:
+        for (var j = 0; j <= 4; j++) {
+            if (num1 < 18) {
+                alert('your answer is too low.');
+                var age = prompt('please reanswer the question? ');
+                var num1 = parseInt(age);
+            } else if (num1 > 30) {
+                alert('your answer is too high');
+                var age = prompt('please reanswer the question? ');
+                var num1 = parseInt(age);
+
+            } else if (num1 === 26) {
+                alert('correct answer  iam ' + num1 + ' years old ');
+                score++;
+            }
+            else {
+                alert('wrong answer')
+                var age = prompt('please reanswer the question? ');
+                var num1 = parseInt(age);
+            }
+        }
+}
+var fruit = ['apple', 'orange', 'banana', 'Avocados', 'Strawberry', 'Watermelon'];
+var userreplies = prompt('what is the fruit that i like ?');
+userreplies = userreplies.toLowerCase();
+for (var h = 0; h < fruit.length; h++){
+switch (userreplies) {
+    case 'apple':
+    case 'orange':
+    case 'banana':
+    case 'Avocados':
+    case 'Strawberry':
+    case 'Watermelon':
+        alert('correct answer ' );
+        var userreplies = prompt('what is the  other fruit that i like ?');
+        score++;
+        break;
+    default:
+        
+            alert('wrong answer');
+            var userreplies = prompt('please reanswer the question? ');
+
+        }
+}
+alert('your score is '+ score)
+alert('the fruit that i like to eat is '+ fruit[0] + ' ,' + fruit[1] + ', ' + fruit[2] + ', ' + fruit[3] + ' ,' + fruit[4] + ' and ' + fruit[5]);
+alert('Bye bye   ' + name + ', I hope that you enjoy visiting my site ');
+console.log('name', name);
+console.log('name', userreplay);
+console.log('name', age);
+console.log('name', userreplies);
+// document.write('<h2>'+score+'</h2>')
