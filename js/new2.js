ab048964e3e464ccac99e2946e3e2167f1f5b09f
ahmadhawamdeh <ahmadhawamdeh313@gmail.com>
2019-10-09 15:59:23 +0300

    add

--- js/new2.js
+++ js/new2.js
@@ -1,14 +1,4 @@
-# My project will be about me , asking about the wrong habits that you will may do every day
- ** describetion of my  project with the new features that i added **
- 1-  write additional java scribt codes useing for loop and arrays
- 1- add  additional information about me 
- 1- add some styling by css 
- the driver was : ahmad alhawamdeh
- the navigator was : dania al-rababah
-
-
- 
- 'use strict';
+'use strict';
 var score = 0;
 var name = prompt(' what is your name?');
 alert('Hi  ' + name + '  welcome to my bage ');
