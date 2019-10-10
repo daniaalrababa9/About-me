1ad6666ca51757d2066ba5bc4954f1d6470289e8
daniaalrababa9 <rababadania@gmail.com>
2019-10-10 14:59:17 +0300

    add

--- js/new2.js
+++ js/new2.js
@@ -33,73 +33,73 @@ function q1(userreply) {
     return (userreply);
 }
 q1();
-function q2(num1){
+function q2(num1) {
     var age = prompt('do you know how old iam? ');
-var num1 = parseInt(age);
+    var num1 = parseInt(age);
 
-switch (num1) {
-    case 26:
-        alert('correct answer  iam ' + num1 + ' years old ');
-        score++;
-        break;
-    default:
-        for (var j = 0; j <= 4; j++) {
-            if (num1 < 18) {
-                alert('your answer is too low.');
-                var age = prompt('please reanswer the question? ');
-                var num1 = parseInt(age);
-            } else if (num1 > 30) {
-                alert('your answer is too high');
-                var age = prompt('please reanswer the question? ');
-                var num1 = parseInt(age);
+    switch (num1) {
+        case 26:
+            alert('correct answer  iam ' + num1 + ' years old ');
+            score++;
+            break;
+        default:
+            for (var j = 0; j <= 4; j++) {
+                if (num1 < 18) {
+                    alert('your answer is too low.');
+                    var age = prompt('please reanswer the question? ');
+                    var num1 = parseInt(age);
+                } else if (num1 > 30) {
+                    alert('your answer is too high');
+                    var age = prompt('please reanswer the question? ');
+                    var num1 = parseInt(age);
 
-            } else if (num1 === 26) {
-                alert('correct answer  iam ' + num1 + ' years old ');
-                score++;
+                } else if (num1 === 26) {
+                    alert('correct answer  iam ' + num1 + ' years old ');
+                    score++;
+                }
+                else {
+                    alert('wrong answer')
+                    var age = prompt('please reanswer the question? ');
+                    var num1 = parseInt(age);
+                }
             }
-            else {
-                alert('wrong answer')
-                var age = prompt('please reanswer the question? ');
-                var num1 = parseInt(age);
-            }
-        }
-}
-    return(num1);
+    }
+    return (num1);
 }
 q2();
 
-function q3(userreplies){
+function q3(userreplies) {
     var fruit = ['apple', 'orange', 'banana', 'Avocados', 'Strawberry', 'Watermelon'];
-var userreplies = prompt('what is the fruit that i like ?');
-userreplies = userreplies.toLowerCase();
-for (var h = 0; h < fruit.length; h++){
-switch (userreplies) {
-    case 'apple':
-    case 'orange':
-    case 'banana':
-    case 'Avocados':
-    case 'Strawberry':
-    case 'Watermelon':
-        alert('correct answer ' );
-        var userreplies = prompt('what is the  other fruit that i like ?');
-        score++;
-        break;
-    default:
+    var userreplies = prompt('what is the fruit that i like ?');
+    userreplies = userreplies.toLowerCase();
+    for (var h = 0; h < fruit.length; h++) {
+        switch (userreplies) {
+            case 'apple':
+            case 'orange':
+            case 'banana':
+            case 'Avocados':
+            case 'Strawberry':
+            case 'Watermelon':
+                alert('correct answer ');
+                score++;
+                h = fruit.length;
+                break;
+            default:
 
-            alert('wrong answer');
-            var userreplies = prompt('please reanswer the question?and choose from these choice (orange , mango ,Cherries,Grapes,apple,banana, Dragon Fruit,Strawberry,Avocados)');
+                alert('wrong answer');
+                var userreplies = prompt('please reanswer the question?and choose from these choice (orange , mango ,Cherries,Grapes,apple,banana, Dragon Fruit,Strawberry,Avocados)');
 
         }
-}
-    return(userreplies);
+    }
+    return (userreplies);
 }
 q3();
 
-alert('your score is '+ score)
-alert('the fruit that i like to eat is '+ fruit[0] + ' ,' + fruit[1] + ', ' + fruit[2] + ', ' + fruit[3] + ' ,' + fruit[4] + ' and ' + fruit[5]);
+alert('your score is ' + score)
+alert('the fruit that i like to eat is ' + fruit[0] + ' ,' + fruit[1] + ', ' + fruit[2] + ', ' + fruit[3] + ' ,' + fruit[4] + ' and ' + fruit[5]);
 alert('Bye bye   ' + name + ', I hope that you enjoy visiting my site ');
 console.log('name', name);
 console.log('userreplay', userreply);
 console.log('age', age);
 console.log('userreplies)', userreplies);
-document.write('<h2>'+score+'</h2>')
+document.write('<h2>' + score + '</h2>')
